b64dfde985a9092a6e7ff42b53735f14bc162b14
Muhammad-Rebaal <mrebaal14@gmail.com>
2023-04-24 16:33:42 +0500

    JavaScript Array

---
+++ Array.js
@@ -0,0 +1,14 @@
+                                        //Array(Data_Structure) 
+
+let zoo = ['tiger','lion','panther']
+
+console.log(zoo)
+console.log(zoo[0])
+console.log(zoo[1])
+console.log(zoo[2])
+console.log(zoo.concat(['elephant','dinasour']))
+console.log(zoo.shift())
+console.log(zoo.pop())
+console.log(zoo.push('panther'))
+console.log(zoo.unshift('tiger'))
+console.log(zoo)
